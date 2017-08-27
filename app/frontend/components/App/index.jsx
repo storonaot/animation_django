606db304bbce610c9d66f85593cbaedaaeec671f
storonaot <storonaot@gmail.com>
2017-08-27 23:15:11 +0300

    added bread crumbs

--- app/frontend/components/App/index.jsx
+++ app/frontend/components/App/index.jsx
@@ -6,17 +6,20 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import BreadCrumbs from '_shared/BreadCrumbs'
 import muiTheme from '../../javascripts/muiTheme'
 
-const App = ({ children, router }) => (
-  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
-    <div>
-      <HeaderComp router={router} />
-      <Grid fluid>
-        <BreadCrumbs />
-        {children}
-      </Grid>
-    </div>
-  </MuiThemeProvider>
-)
+const App = ({ children, router }) => {
+  const crumbs = router.getCurrentLocation().pathname.split('/')
+  return (
+    <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+      <div>
+        <HeaderComp router={router} />
+        <Grid fluid>
+          <BreadCrumbs crumbs={crumbs} />
+          {children}
+        </Grid>
+      </div>
+    </MuiThemeProvider>
+  )
+}
 
 App.propTypes = {
   children: PropTypes.shape({}).isRequired,
--- app/frontend/components/_shared/BreadCrumbs/index.jsx
+++ app/frontend/components/_shared/BreadCrumbs/index.jsx
@@ -1,5 +1,13 @@
+import PropTypes from 'prop-types'
 import styles from './styles'
 
-const BreadCrumbs = () => <div className={styles.breadcrumbs}>Хлебные крошки</div>
+const BreadCrumbs = ({ crumbs }) => {
+  console.log('crumbs', crumbs)
+  return <div className={styles.breadcrumbs}>Хлебные крошки</div>
+}
+
+BreadCrumbs.propTypes = {
+  crumbs: PropTypes.arrayOf(React.PropTypes.string).isRequired
+}
 
 export default BreadCrumbs
